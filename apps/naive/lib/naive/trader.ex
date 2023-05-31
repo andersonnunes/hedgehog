9fe037b77badb7ca2502555d7623fc66b4ba3632
Anderson Nunes <anderson.nunes1988@gmail.com>
2023-05-30 23:33:07 -0300

    Implementation of the second scenario

--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -3,6 +3,7 @@ defmodule Naive.Trader do
 
   require Logger
 
+  alias Decimal, as: D
   alias Streamer.Binance.TradeEvent
 
   defmodule State do
@@ -49,6 +50,35 @@ defmodule Naive.Trader do
     {:noreply, %{state | buy_order: order}}
   end
 
+  def handle_cast(
+        %TradeEvent{
+          buyer_order_id: order_id,
+          quantity: quantity
+        },
+        %State{
+          symbol: symbol,
+          buy_order: %Binance.OrderResponse{
+            price: buy_price,
+            order_id: order_id,
+            orig_qty: quantity
+          },
+          profit_interval: profit_interval,
+          tick_size: tick_size
+        } = state
+      ) do
+    sell_price = calculate_sell_price(buy_price, profit_interval, tick_size)
+
+    Logger.info(
+      "Buy order filled, placing SELL order for " <>
+        "#{symbol}@#{sell_price}, quantity: #{quantity}"
+    )
+
+    {:ok, %Binance.OrderResponse{} = order} =
+      Binance.order_limit_sell(symbol, quantity, sell_price, "GTC")
+
+    {:noreply, %{state | sell_order: order}}
+  end
+
   defp fetch_tick_size(symbol) do
     Binance.get_exchange_info()
     |> elem(1)
@@ -57,5 +87,27 @@ defmodule Naive.Trader do
     |> Map.get("filters")
     |> Enum.find(&(&1["filterType"] == "PRICE_FILTER"))
     |> Map.get("tickSize")
+  end
+
+  def calculate_sell_price(buy_price, profit_interval, tick_size) do
+    fee = "1.001"
+
+    original_price = D.mult(buy_price, fee)
+
+    net_target_price =
+      D.mult(
+        original_price,
+        D.add("1.0", profit_interval)
+      )
+
+    gross_target_price = D.mult(net_target_price, fee)
+
+    D.to_string(
+      D.mult(
+        D.div_int(gross_target_price, tick_size),
+        tick_size
+      ),
+      :normal
+    )
   end
 end
