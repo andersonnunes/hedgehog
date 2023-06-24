ad3a6fd5650da547110e87e303a3df04fde02e9a
Anderson Nunes <anderson.nunes1988@gmail.com>
2023-06-24 09:39:46 -0300

    Remove boilerplate using use macro

--- apps/naive/lib/naive/dynamic_symbol_supervisor.ex
+++ apps/naive/lib/naive/dynamic_symbol_supervisor.ex
@@ -1,78 +1,31 @@
 defmodule Naive.DynamicSymbolSupervisor do
-  use DynamicSupervisor
+  use Core.ServiceSupervisor,
+    repo: Naive.Repo,
+    schema: Naive.Schema.Settings,
+    module: __MODULE__,
+    worker_module: Naive.SymbolSupervisor
 
   require Logger
 
   def start_link(init_arg) do
-    DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+    Core.ServiceSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
   end
 
   def init(_init_arg) do
-    DynamicSupervisor.init(strategy: :one_for_one)
+    Core.ServiceSupervisor.init(strategy: :one_for_one)
   end
 
-  def autostart_workers do
-    Core.ServiceSupervisor.autostart_workers(
-      Naive.Repo,
-      Naive.Schema.Settings,
-      __MODULE__,
-      Naive.SymbolSupervisor
-    )
-  end
-
-  def start_trading(symbol) do
-    Core.ServiceSupervisor.start_worker(
-      symbol,
-      Naive.Repo,
-      Naive.Schema.Settings,
-      __MODULE__,
-      Naive.SymbolSupervisor
-    )
-  end
-
-  def stop_trading(symbol) do
-    Core.ServiceSupervisor.stop_worker(
-      symbol,
-      Naive.Repo,
-      Naive.Schema.Settings,
-      __MODULE__,
-      Naive.SymbolSupervisor
-    )
-  end
-
-  def shutdown_trading(symbol) when is_binary(symbol) do
-    case Core.ServiceSupervisor.get_pid(Naive.SymbolSupervisor, symbol) do
+  def shutdown_worker(symbol) when is_binary(symbol) do
+    case get_pid(symbol) do
       nil ->
-        Logger.warning("#{Naive.SymbolSupervisor} worker for #{symbol} already stopped")
-
-        {:ok, _settings} =
-          Core.ServiceSupervisor.update_status(
-            symbol,
-            "off",
-            Naive.Repo,
-            Naive.Schema.Settings
-          )
+        Logger.warn("#{Naive.SymbolSupervisor} worker for #{symbol} already stopped")
+        {:ok, _settings} = update_status(symbol, "off")
 
       _pid ->
         Logger.info("Initializing shutdown of #{Naive.SymbolSupervisor} worker for #{symbol}")
-
-        {:ok, settings} =
-          Core.ServiceSupervisor.update_status(
-            symbol,
-            "shutdown",
-            Naive.Repo,
-            Naive.Schema.Settings
-          )
-
+        {:ok, settings} = update_status(symbol, "shutdown")
         Naive.Leader.notify(:settings_updated, settings)
         {:ok, settings}
     end
-  end
-
-  def start_symbol_supervisor(symbol) do
-    DynamicSupervisor.start_child(
-      Naive.DynamicSymbolSupervisor,
-      {Naive.SymbolSupervisor, symbol}
-    )
   end
 end
