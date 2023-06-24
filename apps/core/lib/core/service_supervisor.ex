64a4ff748ddbee8a3433029ecd62c75613044be6
Anderson Nunes <anderson.nunes1988@gmail.com>
2023-06-24 08:42:06 -0300

    Pseudo generalize Core.ServiceSupervisor module

---
+++ apps/core/lib/core/service_supervisor.ex
@@ -0,0 +1,59 @@
+defmodule Core.ServiceSupervisor do
+  require Logger
+
+  def autostart_workers do
+    fetch_symbols_to_start()
+    |> Enum.map(&start_worker/1)
+  end
+
+  def start_worker(symbol) when is_binary(symbol) do
+    case get_pid(symbol) do
+      nil ->
+        Logger.info("Starting trading of #{symbol}")
+        {:ok, _settings} = update_status(symbol, "on")
+
+        {:ok, _pid} =
+          DynamicSupervisor.start_child(
+            Naive.DynamicSymbolSupervisor,
+            {Naive.SymbolSupervisor, symbol}
+          )
+
+      pid ->
+        Logger.warning("Trading on #{symbol} already started")
+        {:ok, _settings} = update_status(symbol, "on")
+        {:ok, pid}
+    end
+  end
+
+  def stop_worker(symbol) when is_binary(symbol) do
+    case get_pid(symbol) do
+      nil ->
+        Logger.warning("Trading on #{symbol} already stopped")
+        {:ok, _settings} = update_status(symbol, "off")
+
+      pid ->
+        Logger.info("Stopping trading of #{symbol}")
+
+        :ok =
+          DynamicSupervisor.terminate_child(
+            Naive.DynamicSymbolSupervisor,
+            pid
+          )
+
+        {:ok, _settings} = update_status(symbol, "off")
+    end
+  end
+
+  defp get_pid(symbol) do
+  end
+
+  defp update_status(symbol, status)
+       when is_binary(symbol) and is_binary(status) do
+  end
+
+  defp start_symbol_supervisor(symbol) do
+  end
+
+  defp fetch_symbols_to_start() do
+  end
+end
